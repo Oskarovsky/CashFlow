67f3df3d763822f21c8a6c67dafd0637d21cafcb
Oskar Slyk <oskar.slyk@mavoco.com>
2022-12-20 23:32:27 +0100

    Redis configuration

--- finance-service/build.gradle.kts
+++ finance-service/build.gradle.kts
@@ -26,7 +26,7 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    implementation("redis.clients:jedis:4.3.1")
+    implementation("redis.clients:jedis")
     implementation("io.springfox:springfox-swagger-ui:3.0.0")
     implementation("io.springfox:springfox-swagger2:3.0.0")
 
--- kafka-connector/build.gradle.kts
+++ kafka-connector/build.gradle.kts
@@ -20,7 +20,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-redis")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    implementation("redis.clients:jedis:4.3.1")
+    implementation("redis.clients:jedis")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
